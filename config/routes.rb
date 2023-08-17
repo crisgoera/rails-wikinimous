581a09510b55bde1bae6127c59ac2ee5ac8aeb19
Cristian Goenaga <crisgoera@gmail.com>
2023-08-17 15:09:51 +0200

    Code update

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Read all
+  root 'articles#index'
+
+  # Create
+  get '/articles/new', to: 'articles#new'
+  post '/articles/', to: 'articles#create'
+
+  # Read one
+  get '/articles/:id', to: 'articles#show', as: :article
+
+  # Update
+  get '/articles/:id/edit', to: 'articles#edit', as: :article_edit
+  patch '/articles/:id', to: 'articles#update'
+
+  # Delete
+  delete '/articles/:id', to: 'articles#delete'
 end
